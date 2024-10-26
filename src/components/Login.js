a398ebd7006adfc6c0df1cd7a59b5ced15983144
Anamika Saini <anamika08saini08@gmail.com>
2024-10-26 12:35:15 +0530

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,17 @@ import Header from "./Header"
 
 const Login = () => {
   return (
-    <div>
+    <div className="">
       <Header/>
-      <div>
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/IN-en-20241021-TRIFECTA-perspective_2277eb50-9da3-4fdf-adbe-74db0e9ee2cf_large.jpg" alt="bgImg"/>
+      <div className="absolute">
+        <img className="h-[100vh] w-[100vw]" src="https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/IN-en-20241021-TRIFECTA-perspective_2277eb50-9da3-4fdf-adbe-74db0e9ee2cf_large.jpg" alt="bgImg"/>
       </div>
+      <form className="absolute w-[90%] md:w-3/12 bg-black bg-opacity-75 p-12 mx-auto right-0 left-0 my-36 text-white">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
+        <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
+        <button className="p-4 my-6  bg-red-700 w-full rounded-lg">Sign In</button>
+      </form>
     </div>
   )
 }
